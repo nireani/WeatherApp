9675ea4303112e8e640b92a9ea699f931665b7f1
Nir <nireani@gmail.com>
2021-05-06 00:07:48 +0300

    add heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path")
 const api = require("./server/routes/api")
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost/WeatherApp",{ useFindAndModify: false , useUnifiedTopology: true ,useNewUrlParser: true } )
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WeatherApp",{ useFindAndModify: false , useUnifiedTopology: true ,useNewUrlParser: true } )
 
 
 app.use(express.static(path.join(__dirname, 'dist')))
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')))
 
 app.use('/',api)
 
-const port = 3010
+const port = process.env.PORT ||3010
 app.listen(port , function(){
     console.log(`running on port ${port}`);
 })
